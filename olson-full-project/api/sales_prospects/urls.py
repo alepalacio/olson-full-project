ca81f746163c664d8a208b21a8af09c3e1e66180
alepalacio <g17ada03@hackademy.mx>
2022-04-24 02:09:24 -0300

    created endpoint for prospects signup

--- olson-full-project/api/sales_prospects/urls.py
+++ olson-full-project/api/sales_prospects/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SalesProspectProfileAPIView.as_view(),), 
+    path('create/', views.SalesProspectProfileAPIView.as_view(),), 
 ]
