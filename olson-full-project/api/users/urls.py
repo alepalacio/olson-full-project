c0f0fba50177d727aea91eb387fe6b34310a349b
alepalacio <g17ada03@hackademy.mx>
2022-04-24 16:30:52 -0300

    modified put endpoint to catch id from body, not as url parameter

--- olson-full-project/api/users/urls.py
+++ olson-full-project/api/users/urls.py
@@ -4,7 +4,8 @@ from users import views
 urlpatterns = [
     path('users/', views.UserAPIView.as_view(),),
     path('signup/', views.UserSignUpAPIView.as_view(), name='user_signup',),
-    path('update/<int:pk>/', views.UserDetailAPIView.as_view(), name='user_detail',),
-    path('change_password/<int:pk>/', views.UserChangePasswordAPIView.as_view(),),
-    path('reset_password/<int:pk>/', views.UserResetPasswordAPIView.as_view(),),
+    path('login/', views.UserLoginAPIView.as_view(), name='user_login',),
+    path('update/', views.UserDetailAPIView.as_view(), name='user_detail',),
+    path('change_password/', views.UserChangePasswordAPIView.as_view(),),
+    path('reset_password/', views.UserResetPasswordAPIView.as_view(),),
 ]
